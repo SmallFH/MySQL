22872a3ccc029e4a8541c79cc6659781ba7f1af9
Smile-FH <fu_hao_email@163.com>
2020-03-31 18:37:10 +0800

    这是一次测试

--- src/com/sql/util/GetMySQLUtil.java
+++ src/com/sql/util/GetMySQLUtil.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class GetMySQLUtil {
 	private static final String DIR = "com.mysql.cj.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/school?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Shanghai";
-	private static final String USER = "root";
-	private static final String PASS = "584261";
+	private static final String USER = "Smile_FH";
+	private static final String PASS = "Fh0816.";
 
 	public static Connection getMySQL() {
 		Connection connection = null;
